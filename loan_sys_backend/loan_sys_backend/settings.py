7db068a59a420f6bf1c39682d9b8a61974e54158
jbosama <julleibibjb@gmail.com>
2023-03-14 22:02:11 +0300

    added authentication

--- loan_sys_backend/loan_sys_backend/settings.py
+++ loan_sys_backend/loan_sys_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -117,9 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+MEDIA_URL = '/media/'
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS=[ 
+    os.path.join(BASE_DIR,'static')
+]
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
